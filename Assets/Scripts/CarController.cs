aaec1aa60ee305b9c517f577d9b659de59e483ef
Ramon Moura <ramonmourarsm@outlook.com>
2018-04-07 16:24:42 -0300

    Ajustes

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+	private bool move = false;
+	public Rigidbody2D rigidbody2DCar;
+	public float speed = 20f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		move = Input.GetButton("Fire1");
+	}
+
+	void FixedUpdate(){
+		
+		if(move){
+			rigidbody2DCar.AddForce(transform.right * speed * Time.fixedDeltaTime * 100f, ForceMode2D.Force);
+		}
+	}
+}
